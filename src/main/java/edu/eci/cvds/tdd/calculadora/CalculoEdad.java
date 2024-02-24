23d18658518c869f69484015ccbf895cd95273de
unknown <milton.gutierrez-l@labinfo.is.escuelaing.edu.co>
2024-02-24 08:27:59 -0500

    Pruebas pasando, implementacion de clases pedidas

--- src/main/java/edu/eci/cvds/tdd/calculadora/CalculoEdad.java
+++ src/main/java/edu/eci/cvds/tdd/calculadora/CalculoEdad.java
@@ -4,8 +4,9 @@ import edu.eci.cvds.tdd.registry.Person;
 
 public class CalculoEdad {
 
-    public static boolean esMenorDeEdad(Person p){
+    public static boolean esMenorDeEdad(Person p) throws Exception{
+        if(p.getAge() < 0) throw new Exception("Edad invalida");
+        if(p.getAge() >= 0 && p.getAge() < 18) return true;
         return false;
-        
     }
 }
--- src/test/java/edu/eci/cvds/tdd/calculadora/CalculadoraEdadTest.java
+++ src/test/java/edu/eci/cvds/tdd/calculadora/CalculadoraEdadTest.java
@@ -1,5 +1,6 @@
 package edu.eci.cvds.tdd.calculadora;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -10,9 +11,40 @@ import edu.eci.cvds.tdd.registry.Person;
 public class CalculadoraEdadTest {
 
     @Test
-    public void shouldReturnFalseWhenPersonIsAnAdult(){
+    public void shouldReturnFalseWhenPersonIsAnAdult() throws Exception{
+        //Edad primer limite: 18
         Person p = new Person("Hola", 1, 18, Gender.MALE, true);
         assertFalse(CalculoEdad.esMenorDeEdad(p));
+        //Edad media: 50
+        Person p2 = new Person("Hola", 2, 50, Gender.MALE, true);
+        assertFalse(CalculoEdad.esMenorDeEdad(p2));
+        //Edad segundo limite: 150
+        Person p3 = new Person("Hola", 3, 150, Gender.MALE, true);
+        assertFalse(CalculoEdad.esMenorDeEdad(p3));
+
+
     }
 
+    @Test
+    public void shouldReturnTrueWhenPersonIsUnderage() throws Exception{
+        //Edad primer limite: 0
+        Person p = new Person("Hola", 1, 0, Gender.MALE, true);
+        assertTrue(CalculoEdad.esMenorDeEdad(p));
+        //Edad media: 10
+        Person p2 = new Person("Hola", 2, 10, Gender.MALE, true);
+        assertTrue(CalculoEdad.esMenorDeEdad(p2));
+        //Edad segundo limite: 17
+        Person p3 = new Person("Hola", 3, 17, Gender.MALE, true);
+        assertTrue(CalculoEdad.esMenorDeEdad(p3));
+    }
+
+    @Test(expected = Exception.class)
+    public void shouldFailWhenPersonHasInvalidValuesForAge() throws Exception{
+        Person p = new Person("Hola", 1, -1, Gender.MALE, true);
+        Person p2 = new Person("Hola", 3, -150, Gender.MALE, true);
+        boolean result;
+        result = CalculoEdad.esMenorDeEdad(p);
+        result = CalculoEdad.esMenorDeEdad(p2);
+    }
+    
 }
